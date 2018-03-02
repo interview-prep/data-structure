ff0013a17c2742ca076cb02287e67f235008e85b
Sandeep.M <Sandeep.M@non.schneider-electric.com>
2018-03-02 12:19:07 +0530

    transpose matrix

--- src/main/java/com/sandeep/interview/collections/arrays/ArrayTranspose.java
+++ src/main/java/com/sandeep/interview/collections/arrays/ArrayTranspose.java
@@ -10,23 +10,34 @@ public class ArrayTranspose {
         int x[][] = new int[3][3];
         int s[] = new int[3];
         System.out.println("matrix length : " + x.length);
+        System.out.println("[Original 3 by 3 matrix]");
+        System.out.println("---------------------------");
+
         for (int i = 0; i < 3; i++) {
             for (int j = 0; j < 3; j++) {
                 x[i][j] = i + j + new Random().nextInt(6);
-            }
-        }
-
-        System.out.println("[Original 3 by 3 matrix]");
-        System.out.println("---------------------------");
-        for (int i = 0; i < 3; i++) {
-            System.out.print("row " + i +" => ");
-            for (int j = 0; j < 3; j++) {
-                System.out.printf("%-3d", x[i][j]);
+                System.out.print(x[i][j] + " ");
             }
             System.out.println();
         }
-        //TODO transpose
+
+        //TODO transpose working on it
+        System.out.println();
+        int y[][] = new int[3][3];
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                y[j][i] = x[i][j];
+            }
+        }
+
         System.out.println("\n[Transposed 3 by 3 matrix]");
         System.out.println("--------------------------------");
+        for (int i = 0; i < 3; i++) {
+            System.out.print("column " + i + " => ");
+            for (int j = 0; j < 3; j++) {
+                System.out.print(y[i][j] + " ");
+            }
+            System.out.println();
+        }
     }
 }
