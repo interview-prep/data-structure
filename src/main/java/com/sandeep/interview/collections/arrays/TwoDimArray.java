155b3e98a0452b67335e1ba02d836ea81ae7a8b7
Sandeep.M <Sandeep.M@non.schneider-electric.com>
2018-03-02 10:56:50 +0530

    two dimensional

--- src/main/java/com/sandeep/interview/collections/arrays/TwoDimArray.java
+++ src/main/java/com/sandeep/interview/collections/arrays/TwoDimArray.java
@@ -9,7 +9,7 @@ public class TwoDimArray {
     public static void main(String[] args) {
 
         int x[][] = new int[3][3];
-
+        int s[] = new int[3];
         System.out.println(x.length);
         for (int i = 0; i < 3; i++) {
             for (int j = 0; j < 3; j++) {
@@ -23,21 +23,31 @@ public class TwoDimArray {
             for (int j = 0; j < 3; j++) {
                 System.out.printf("%-3d", x[i][j]);
                 sum += x[i][j];
-            }
-            System.out.println("= " + sum);
-        }
-        System.out.println("===========================");
-// find sum of each column
-        for (int i = 0; i < 3; i++) {
-            int sum = 0;
-            for (int j = 0; j < 3; j++) {
-                System.out.printf("%-3d", x[j][i]);
-                sum += x[j][i];
+
             }
             System.out.println("= " + sum);
         }
 
-        System.out.println("===========================");
+
+        System.out.println("===============\n\n");
+// find sum of each column
+        for (int i = 0; i < 3; i++) {
+            int sum = 0;
+            for (int j = 0; j < 3; j++) {
+                System.out.printf("%-3d", x[i][j]);
+                sum += x[j][i];
+                s[i] = sum;
+            }
+            System.out.println();
+        }
+        for (int i = 0; i < 3; i++) {
+            System.out.printf("%-3s", "_");
+        }
+        System.out.println();
+        for (int i = 0; i < 3; i++) {
+            System.out.printf("%-3d", s[i]);
+        }
+        System.out.println("\n================");
 
 
     }
