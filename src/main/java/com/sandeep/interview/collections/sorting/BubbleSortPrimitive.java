4adc343b5e883d37b47ea1039ad497c5d5923b55
Sandeep.M <Sandeep.M@non.schneider-electric.com>
2018-03-02 11:33:53 +0530

    code refactor

--- src/main/java/com/sandeep/interview/collections/sorting/BubbleSortPrimitive.java
+++ src/main/java/com/sandeep/interview/collections/sorting/BubbleSortPrimitive.java
@@ -17,12 +17,9 @@ public class BubbleSortPrimitive {
                 }
             }
         }
-        System.out.println(arr[0]);
-        System.out.println(arr[1]);
-        System.out.println(arr[2]);
-        System.out.println(arr[3]);
-        System.out.println(arr[4]);
-        System.out.println(arr[5]);
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i]);
+        }
         return arr;
     }
 
